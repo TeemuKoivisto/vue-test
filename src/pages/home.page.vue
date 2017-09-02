51f20a0dc7a8777edd928fd01f868cf5f1387db9
Teemu Koivisto <tkoivisto456@gmail.com>
2017-09-02 15:44:30 +0300

    Create home.page

---
+++ src/pages/home.page.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="main__container">
+    <h1>hei olen etusivu</h1>
+    <p class="home__text">{{ yo }}</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'home-page',
+  data: () => ({
+    msg: 'yo'
+  })
+}
+</script>
+
+<style lang="scss">
+.main {
+  &__container {
+    border: 1px solid black;
+  }
+}
+.home__text {
+  padding: 0;
+  margin: 0;
+}
+</style>
