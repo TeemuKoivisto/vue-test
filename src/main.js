e05197cd0b85bd8c9c5b8f04cf74587d3d2d06ad
Teemu Koivisto <tkoivisto456@gmail.com>
2017-09-02 19:39:19 +0300

    Move router to main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App'
-import router from './router'
+import HomePage from './pages/Home.page'
 
 import NavBar from '@/components/ui/Navbar.component'
 Vue.component('x-navbar', NavBar)
+
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  base: '',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    }
+  ]
+})
 
 Vue.config.productionTip = false
 
